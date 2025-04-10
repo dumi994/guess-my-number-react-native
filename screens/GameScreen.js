5e92499ec257079190e84b72c7e63386cfbca79b
Dumi <dumi.9944@gmail.com>
2025-04-10 15:23:44 +0200

    aggiunta larghezza fissa al title component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -114,7 +114,8 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 12,
+    padding: 24,
+    alignItems: "center",
   },
   buttonsContainer: {
     flexDirection: "row",
